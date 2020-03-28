44d1eb92f8dd2c68deca431461fc157f472d836a
pogikeith <pogikeith@yahoo.com>
2020-03-28 17:08:46 -0600

    challenge2

---
+++ Bld8Apps/var.py
@@ -0,0 +1,27 @@
+x = 5 
+print(x)      
+x= '5'
+print(x)
+
+x=5
+y=5
+print(x+y)
+
+print(24)
+
+age = 5
+"i am " + str(age)
+print(age) 
+print('i am {} years old'.format(age))
+
+age = 5
+"i am " + str(age)
+print('i am {} years old and {} months old'.format(age, 8))
+
+
+print('i am {age} years old and {age} months old'.format(age=age))
+
+friend_name = 'Anne'
+friend_age = 30
+
+multi_format_phrase = "Hey {}, are you {age} years old?".format(friend_name, age = friend_age)
---
+++ home.py
@@ -0,0 +1,61 @@
+number = 3
+print(number)
+
+number = 2 * number
+print(number)
+
+number = number * number
+print(number)
+
+
+score = 50
+print(score)
+
+score = score + 20
+print(score)
+
+
+score = 50
+print(score)
+
+score += 20
+print(score)
+
+
+d= 2
+d *=3+4
+print(d)
+
+number = 10
+number *= number 
+print(number)
+
+x=7 
+x+=2
+print(x)
+
+x=7
+x-=2
+print(x)
+
+x= 7
+x*=7
+print(x)
+
+x=7
+x/=7
+print(x)
+
+x=7
+x//=7
+print(x)
+
+x=11
+x%=7
+print(x)
+
+x=5
+x**=2
+print(x)
+
+ 
