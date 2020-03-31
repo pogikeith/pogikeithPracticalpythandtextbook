428279ed1445c33131ecaee19f29b518f04d8fec
pogikeith <pogikeith@yahoo.com>
2020-03-30 23:06:00 -0600

    inputs

---
+++ Bld8Apps/inputs.py
@@ -0,0 +1,6 @@
+# input(' enter your age ')
+
+user_age = input('enter your age:  ')
+int(user_age) * 365 *24 *60 *60 
+seconds = int(user_age) * 365 *24 *60 *60
+print('you have lived for {} seconds.'.format(seconds))
