17fd03ad7ab511e5407eaf2d3d6041f7b637c2e8
pogikeith <pogikeith@yahoo.com>
2020-04-14 21:41:53 -0600

    print

--- Book/CH3/ch.4/exer.py
+++ Book/CH3/ch.4/exer.py
@@ -1,2 +1,30 @@
 print(len('albert einstein'))
 print(len('123'))
+
+word1 = 'keith '
+word2= 'slentz'
+print(word1+word2)
+
+word1 = 'four score and '
+word2= 'years ago '
+
+print(word1 + str(7) +word2)
+
+print(int(11))
+print('at' *5)
+print('She said, "That\'s hard to read."')
+
+print('''one
+...two
+...three''')
+
+print('and our understanding of ourselves')
+
+print('one\ttwo\nthree\tfour')
+
+print('one\ntwo', 'three\n', 'four ')
+
+radius = 5
+print("The diameter of the circle is", radius * 2, "cm.")
+
+print(1,2,3)
